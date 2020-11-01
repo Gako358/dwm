e5fc12bfb402c952497756b6aa0afce64b1f7c87
Gako358 <gako358@outlook.com>
2020-11-01 12:08:42 +0100

    center added

--- config.def.h
+++ config.def.h
@@ -59,9 +59,14 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     iscentered   isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           -1 },
+	/* class           instance    title       tags mask     iscentered   isfloating   monitor */
+	{ "Gimp",           NULL,       NULL,       0,            1,             1,           -1 },
+	{ "Pcmanfm",        NULL,       NULL,       0,            1,             1,           -1 },
+	{ "Brave",	        NULL,       NULL,       1 << 4,       0,             0,            1 },
+	{ "discord",        NULL,       NULL,       1 << 8,       0,             0,            1 },
+	{ "Mailspring",     NULL,       NULL,       1 << 5,       0,             0,            1 },
+	{ "lyx",            NULL,       NULL,       1 << 2,       0,             0,            2 },
+	{ "Virt-manager",   NULL,       NULL,       1 << 7,       0,             0,            2 },
 };
 
 /* layout(s) */
