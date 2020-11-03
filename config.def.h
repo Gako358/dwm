164237fe83671dc592fbed475949c4e7858d381d
Gako358 <gako358@outlook.com>
2020-11-03 08:09:47 +0100

    light scheme dmenu

--- config.def.h
+++ config.def.h
@@ -19,14 +19,14 @@ static const char dmenufont[]               = "monospace:size=14";
 /* colors */
 static const char col_bg[]                  = "#282828";
 static const char col_fg[]                  = "#ebdbb2";
-static const char col_red[]                 = "#cc241d";
+static const char col_red[]                 = "#fb4934";
 static const char col_gray[]                = "#504945";
 static const char col_dark[]                = "#3c3836";
-static const char col_blue[]                = "#458588";
-static const char col_cyan[]                = "#689d6a";
-static const char col_green[]               = "#98971a";
-static const char col_yellow[]              = "#d79921";
-static const char col_purple[]              = "#b16286";
+static const char col_blue[]                = "#83a598";
+static const char col_cyan[]                = "#8ec07c";
+static const char col_green[]               = "#b8bb26";
+static const char col_yellow[]              = "#fabd2f";
+static const char col_purple[]              = "#d3869b";
 
 static const char *colors[][3]      = {
 	/*					            fg         bg       border   */
@@ -89,7 +89,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_dark, "-nf", col_fg, "-sb", col_blue, "-sf", col_bg, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_fg, "-nf", col_bg, "-sb", col_red, "-sf", col_gray, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
