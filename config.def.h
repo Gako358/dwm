77bcaf891d1d41e9858037b2fd6477b3056d8992
Gako358 <gako358@outlook.com>
2020-11-10 17:37:42 +0100

    browser tag

--- config.def.h
+++ config.def.h
@@ -59,8 +59,8 @@ static const Rule rules[] = {
 	/* class           instance    title       tags mask     iscentered   isfloating   monitor */
 	{ "Gimp",           NULL,       NULL,       0,            1,             1,           -1 },
 	{ "Pcmanfm",        NULL,       NULL,       0,            1,             1,           -1 },
-	{ "Brave",	        NULL,       NULL,       1 << 1,       0,             0,            1 },
-	{ "discord",        NULL,       NULL,       1 << 3,       0,             0,            1 },
+	{ "Brave",	        NULL,       NULL,       1 << 0,       0,             0,            1 },
+	{ "discord",        NULL,       NULL,       1 << 2,       0,             0,            1 },
 	{ "lyx",            NULL,       NULL,       1 << 4,       0,             0,            2 },
 	{ "Virt-manager",   NULL,       NULL,       1 << 6,       0,             0,            2 },
 };
