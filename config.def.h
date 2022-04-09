81a91a16c27bee7e6bb7a96a9e97a2f41a075c9f
Gako358 <gako358@outlook.com>
2022-04-09 22:31:40 +0200

    fix class for eww

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
-static const char *altbarclass      = "eww"; /* Alternate bar class name */
+static const char *altbarclass      = "eww-apps"; /* Alternate bar class name */
 static const char *altbarcmd        = "$HOME/.config/eww/launch_bar"; /* Alternate bar launch command */
 static const char font[]            = "JetBrainsMono Nerd Font:size=13:antialias=true:autohint=true";
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12:antialias=true:autohint=true";
