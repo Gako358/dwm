872f7775a0fd5f722447a2a7fe91d20d248b5785
Gako358 <gako358@outlook.com>
2022-04-10 11:14:58 +0200

    debugging alt bar ipc conn

--- config.def.h
+++ config.def.h
@@ -5,13 +5,13 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 19;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 19;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 19;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappoh    = 28;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 19;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
-static const char *altbarclass      = "eww-apps"; /* Alternate bar class name */
+static const char *altbarclass      = "eww"; /* Alternate bar class name */
 static const char *altbarcmd        = "$HOME/.config/eww/launch_bar"; /* Alternate bar launch command */
 static const char font[]            = "JetBrainsMono Nerd Font:size=13:antialias=true:autohint=true";
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12:antialias=true:autohint=true";
@@ -51,7 +51,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     iscentered   isfloating CenterThisWindow    monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           1,             0,                  -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,             0,                  -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           0,             1,                   2},
     { NULL,       "spterm",   NULL,       SPTAG(0),     0,           1,             0,                  -1 },
     { NULL,       "ncmpcpp",  NULL,       SPTAG(1),     0,           1,             0,                  -1 },
     { NULL,       "htop",     NULL,       SPTAG(2),     0,           1,             0,                  -1 },
@@ -118,7 +118,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -131,7 +131,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,            			XK_y,  	   togglescratch,  {.ui = 0 } },
+	{ MODKEY,            			XK_grave,  togglescratch,  {.ui = 0 } },
 	{ MODKEY,            			XK_u,	   togglescratch,  {.ui = 1 } },
 	{ MODKEY,            			XK_x,	   togglescratch,  {.ui = 2 } },
 	TAGKEYS(                        XK_1,                      0)
