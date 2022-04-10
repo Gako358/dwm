bbabca9f8f6a7cf0c9878dc9764c26d6075ad89e
Gako358 <gako358@outlook.com>
2022-04-10 10:42:25 +0200

    added support for IPC

--- config.mk
+++ config.mk
@@ -20,9 +20,13 @@ FREETYPEINC = /usr/include/freetype2
 # OpenBSD (uncomment)
 #FREETYPEINC = ${X11INC}/freetype2
 
+# yajl
+YAJLLIBS = -lyajl
+YAJLINC = /usr/include/yajl
+
 # includes and libs
-INCS = -I${X11INC} -I${FREETYPEINC} `pkg-config --cflags xft pango pangoxft`
-LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} -lmpdclient ${FREETYPELIBS} `pkg-config --libs xft pango pangoxft`
+INCS = -I${X11INC} -I${FREETYPEINC} `pkg-config --cflags xft pango pangoxft` -I${YAJLINC}
+LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} -lmpdclient ${FREETYPELIBS} `pkg-config --libs xft pango pangoxft` ${YAJLLIBS}
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
