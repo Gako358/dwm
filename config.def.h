80a487992b8080d91ee9241c1e09fadde77eb281
gako358 <gako358@outlook.com>
2022-08-15 22:43:13 +0200

    resizing dropdowns

--- config.def.h
+++ config.def.h
@@ -58,9 +58,9 @@ typedef struct {
 } Sp;
 const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x37", NULL };
 const char *spcmd2[] = {"st", "-n", "ncmpcpp", "-g", "120x35", "-e", "ncspot", NULL };
-const char *spcmd3[] = {"st", "-n", "mutt", "-g", "144x72", "-e", "neomutt", NULL };
-const char *spcmd4[] = {"st", "-n", "htop", "-g", "144x72", "-e", "btop", NULL };
-const char *spcmd5[] = {"st", "-n", "weechat", "-g", "144x72", "-e", "weechat", NULL };
+const char *spcmd3[] = {"st", "-n", "mutt", "-g", "144x46", "-e", "neomutt", NULL };
+const char *spcmd4[] = {"st", "-n", "htop", "-g", "144x46", "-e", "btop", NULL };
+const char *spcmd5[] = {"st", "-n", "weechat", "-g", "144x46", "-e", "weechat", NULL };
 static Sp scratchpads[] = {
 	/* name          cmd  */
 	{"spterm",      spcmd1},
