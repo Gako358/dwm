a819d931048d5cb1e3960003777f318b81562d69
gako358 <gako358@outlook.com>
2022-08-18 13:30:15 +0200

    adding qacl to rules, float

--- config.def.h
+++ config.def.h
@@ -97,6 +97,7 @@ static const Rule rules[] = {
     /* class      instance    title       tags mask     iscentered   isfloating   monitor */
     { "Gimp",         NULL,       NULL,       0,            1,           1,           -1 },
     { "Joplin",       NULL,       NULL,       0,            1,           1,           -1 },
+    { "Qalculate-gtk",NULL,       NULL,       0,            1,           1,           -1 },
     { "Firefox",      NULL,       NULL,       1,            0,           0,           -1 },
     { "thunderbird",  NULL,       NULL,       1 << 3,       1,           1,           -1 },
     { "discord",      NULL,       NULL,       1 << 4,       0,           0,           -1 },
