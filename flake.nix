fbc17c06edd23d2806275271ab6710f4451ab254
Gako358 <gako358@outlook.com>
2022-10-20 21:18:51 +0200

    nix build environment ready

--- flake.nix
+++ flake.nix
@@ -3,25 +3,48 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: 
-    let 
+  outputs =
+    { self
+    , nixpkgs
+    ,
+    }:
+    let
       system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
       overlay = final: prev: {
         dwm = prev.dwm.overrideAttrs (old: {
-	  src = builtins.path { path = ./.; name = "dwm"; };
-	  buildInputs = old.buildInputs ++ [
-	    prev.imlib2
-	  ];
-	});
+          src = builtins.path {
+            path = ./.;
+            name = "dwm";
+          };
+          buildInputs =
+            old.buildInputs
+            ++ [
+              prev.imlib2
+            ];
+        });
       };
     in
     {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        name = "dwm-dev";
+        packages = with pkgs; [
+          gcc
+          pkgconfig
+          imlib2
+          xorg.libX11
+          xorg.libXft
+          xorg.libXinerama
+        ];
+      };
+
       overlays.default = overlay;
-      checks.${system}.build = (
-        import nixpkgs {
-	  inherit system;
-	  overlays = [ overlay ];
-	}
-      ).dwm;
+      checks.${system}.build =
+        (
+          import nixpkgs {
+            inherit system;
+            overlays = [ overlay ];
+          }
+        ).dwm;
     };
 }
