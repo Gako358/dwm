94fb019d7238639016a7ce83aca5e8c5b198f1fe
Gako358 <gako358@outlook.com>
2022-10-23 14:01:37 +0200

    Revert "removed quit function, fixed compiler warnings and bumped to v6.3"
    This reverts commit 806eb640e09bbecd8b68e074905597d583071710.

--- dwm.c
+++ dwm.c
@@ -293,7 +293,7 @@ static Client *nexttiled(Client *c);
 static void placemouse(const Arg *arg);
 static void pop(Client *);
 static void propertynotify(XEvent *e);
-static void restart(const Arg *arg);
+static void quit(const Arg *arg);
 static Client *recttoclient(int x, int y, int w, int h);
 static Monitor *recttomon(int x, int y, int w, int h);
 static void removesystrayicon(Client *i);
@@ -325,6 +325,8 @@ static void show(Client *c);
 static void showhide(Client *c);
 static void showtagpreview(int tag);
 static void sigchld(int unused);
+static void sighup(int unused);
+static void sigterm(int unused);
 static void spawn(const Arg *arg);
 static void switchtag(void);
 static Monitor *systraytomon(Monitor *m);
@@ -396,6 +398,7 @@ static void (*handler[LASTEvent])(XEvent *) = {
     [ResizeRequest] = resizerequest,
     [UnmapNotify] = unmapnotify};
 static Atom wmatom[WMLast], netatom[NetLast], xatom[XLast];
+static int restart = 0;
 static int running = 1;
 static Cur *cursor[CurLast];
 static Clr **scheme, clrborder;
@@ -651,9 +654,9 @@ void buttonpress(XEvent *e) {
 			}
 	}
 
-	if (ev->x > selmon->ww - (int)TEXTW(stext))
+	if (ev->x > selmon->ww - TEXTW(stext))
          click = ClkStatusText;
-  else
+    else
          click = ClkWinTitle;
     	}
 	if(ev->window == selmon->tabwin) {
@@ -730,7 +733,6 @@ void cleanup(void) {
     drw_cur_free(drw, cursor[i]);
   for (i = 0; i < LENGTH(colors) + 1; i++)
     free(scheme[i]);
-  free(scheme);
   XDestroyWindow(dpy, wmcheckwin);
   drw_free(drw);
   XSync(dpy, False);
@@ -1466,9 +1468,6 @@ void drawbar(Monitor *m) {
 
   if (showsystray && m == systraytomon(m))
     stw = getsystraywidth();
-
-  if (!m->showbar)
-    return;
 
   /* draw status first so it can be overdrawn by tags later */
   if (m == selmon) { /* status is only drawn on selected monitor */
@@ -2466,7 +2465,21 @@ void propertynotify(XEvent *e) {
   }
 }
 
-void restart(const Arg *arg) {
+void quit(const Arg *arg) {
+  if(arg->i == 0)  system("killall bar.sh");
+
+  else if (arg->i)
+    restart = 1;
+
+	Monitor *m;
+	Client *c;
+	for (m = mons; m; m = m->next) {
+		if (m) {
+			for (c = m->stack; c; c = c->next)
+				if (c && HIDDEN(c)) show(c);
+		}
+	}
+
   running = 0;
 }
 
@@ -2869,6 +2882,9 @@ void setup(void) {
   /* clean up any zombies immediately */
   sigchld(0);
 
+  signal(SIGHUP, sighup);
+  signal(SIGTERM, sigterm);
+
   /* init screen */
   screen = DefaultScreen(dpy);
   sw = DisplayWidth(dpy, screen);
@@ -3030,6 +3046,16 @@ void sigchld(int unused) {
     die("can't install SIGCHLD handler:");
   while (0 < waitpid(-1, NULL, WNOHANG))
     ;
+}
+
+void sighup(int unused) {
+  Arg a = {.i = 1};
+  quit(&a);
+}
+
+void sigterm(int unused) {
+  Arg a = {.i = 0};
+  quit(&a);
 }
 
 #define SPAWN_CWD_DELIM " []{}()<>\"':"
@@ -3841,6 +3867,8 @@ int main(int argc, char *argv[]) {
 #endif /* __OpenBSD__ */
   scan();
   run();
+  if (restart)
+    execvp(argv[0], argv);
   cleanup();
   XCloseDisplay(dpy);
   return EXIT_SUCCESS;
