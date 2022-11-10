9560e7fd1e489496b97ce5b5224328eabf91a9a1
Gako358 <gako358@outlook.com>
2022-11-10 19:47:06 +0100

    edit fg and bg

--- config.def.h
+++ config.def.h
@@ -33,8 +33,8 @@ static const char *fonts[]          = { "JetBrainsMono Nerd Font:style:medium:si
 
 // theme
 static const char black[]       = "#282828";
-static const char gray2[]       = "#3B4252"; // unfocused window border
-static const char gray3[]       = "#606672";
+static const char gray2[]       = "#2e323a"; // unfocused window border
+static const char gray3[]       = "#545862";
 static const char gray4[]       = "#6d8dad";
 static const char blue[]        = "#81A1C1";  // focused window border
 static const char green[]       = "#89b482";
@@ -42,7 +42,7 @@ static const char red[]         = "#d57780";
 static const char orange[]      = "#caaa6a";
 static const char yellow[]      = "#EBCB8B";
 static const char pink[]        = "#e39a83";
-static const char col_borderbar[]  = "#2A303C"; // inner border
+static const char col_borderbar[]  = "#282828"; // inner border
 
 static const char *colors[][3]      = {
     /*                     fg       bg      border */
