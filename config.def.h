70e497fac2c1478e5b073c27a3a3689140084a79
Gako358 <gako358@outlook.com>
2022-11-10 19:36:01 +0100

    removed themes and inline in config from now

--- config.def.h
+++ config.def.h
@@ -32,7 +32,17 @@ static const char *fonts[]          = { "JetBrainsMono Nerd Font:style:medium:si
                                         "Material Design Icons-Regular:size=10" };
 
 // theme
-#include "themes/nord.h"
+static const char black[]       = "#16161D";
+static const char gray2[]       = "#3B4252"; // unfocused window border
+static const char gray3[]       = "#606672";
+static const char gray4[]       = "#6d8dad";
+static const char blue[]        = "#81A1C1";  // focused window border
+static const char green[]       = "#89b482";
+static const char red[]         = "#d57780";
+static const char orange[]      = "#caaa6a";
+static const char yellow[]      = "#EBCB8B";
+static const char pink[]        = "#e39a83";
+static const char col_borderbar[]  = "#2A303C"; // inner border
 
 static const char *colors[][3]      = {
     /*                     fg       bg      border */
