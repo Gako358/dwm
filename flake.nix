f16e9d6f6c9cbe844f0ef13a88d0d9a141cfa5d0
Gako358 <gako358@outlook.com>
2023-05-07 12:43:13 +0200

    update flake to use flake utils

--- flake.nix
+++ flake.nix
@@ -1,48 +1,67 @@
 {
   description = "MerrinX dwm build";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs = {
     self,
     nixpkgs,
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
-    overlay = final: prev: {
-      dwm = prev.dwm.overrideAttrs (old: {
-        src = builtins.path {
-          path = ./.;
-          name = "dwm";
-        };
-        buildInputs =
-          old.buildInputs
-          ++ [
-            prev.imlib2
-          ];
-      });
-    };
-  in {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      name = "dwm-dev";
-      packages = with pkgs; [
-        gcc
-        pkgconfig
-        imlib2
-        xorg.libX11
-        xorg.libXft
-        xorg.libXinerama
-      ];
-    };
-
-    overlays.default = overlay;
-    checks.${system}.build =
-      (
-        import nixpkgs {
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
           inherit system;
-          overlays = [overlay];
-        }
-      )
-      .dwm;
-  };
+          overlays = [
+            (final: prev: {
+              dwm = prev.dwm.overrideAttrs (old: {
+                src = builtins.path {
+                  path = ./.;
+                  name = "dwm";
+                };
+                buildInputs =
+                  old.buildInputs
+                  ++ [
+                    prev.imlib2
+                  ];
+              });
+            })
+          ];
+        };
+
+        overlay = final: prev: {
+          dwm = prev.dwm;
+        };
+      in rec {
+        apps = {
+          dwm = {
+            type = "app";
+            program = "${defaultPackage}/bin/dwm";
+          };
+        };
+
+        devShell = pkgs.mkShell {
+          name = "dwm-dev";
+          packages = with pkgs; [
+            gcc
+            pkgconfig
+            imlib2
+            xorg.libX11
+            xorg.libXft
+            xorg.libXinerama
+          ];
+        };
+
+        packages.dwm = pkgs.dwm;
+        defaultApp = apps.dwm;
+        defaultPackage = packages.dwm;
+
+        overlays = {
+          default = overlay;
+        };
+      }
+    );
 }
